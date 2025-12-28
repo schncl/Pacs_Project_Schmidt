dcc00d0d351fc67fb8b534d6cc618d83932780a7
schncl <niccolo.schmidt@mail.polimi.it>
2025-12-28 12:15:29 +0100

    Added command line arguments for plot and config files

--- src/main.py
+++ src/main.py
@@ -2,12 +2,31 @@ from modelnn import ModelNN
 from modelGPC import GPModel
 from config import load_config
 from dataset import MatDataset
+import argparse
 
-if __name__ == "__main__":
 
 
-    config=load_config("configs/config.json")
-    dataset = MatDataset(config["dataset_file"])
+def parse_args():
+    """Parser for command line arguments"""
+    parser=argparse.ArgumentParser(description="ML training specs")
+    parser.add_argument(
+        '--config',
+        type=str,
+        default="../configs/config.json"
+    )
+    parser.add_argument(
+        "--plots",
+        default=False
+    )
+    return parser.parse_args()
+
+
+def main():
+
+    args=parse_args()
+    config=load_config(args.config)
+
+    dataset = MatDataset(config["dataset_path"])
     dataset.load()
 
     X,Y = dataset.prepare_ml_data( config['input_fields'], config['target_fields'])
@@ -19,28 +38,29 @@ if __name__ == "__main__":
 
     X_test,Y_test = model.train_model(X, Y)
 
-
-    result = model.predict(X_test)
-    print(result)
-
-    model.plot_training()
-    model.plot_confusions(X_test,Y_test,target_names)
+  
+    if  args.plots:
+        model.plot_training()
+        model.plot_confusions(X_test,Y_test,target_names)
+        model.visualize_predictions()
+    
     metrics = model.compute_metrics(X_test,Y_test,target_names)
-    model.visualize_predictions()
-
 
     model.save("model_family1.pt")
-
+    
     model2=GPModel(config['gaussian_process'])
 
     X_test,Y_test=model2.train_model(X, Y)
 
-    result_gp = model2.predict(X_test)
-    print(result_gp)
-    
-    model2.plot_confusions(X_test,Y_test,target_names)
+    if not args.noplots:
+        model2.plot_confusions(X_test,Y_test,target_names)
+        metrics2=model2.compute_metrics(X_test,Y_test,target_names)
+        model2.visualize_predictions()
+            
     metrics2=model2.compute_metrics(X_test,Y_test,target_names)
-    model2.visualize_predictions()
-
     model2.save("model_family1_GPC.joblib")
+
+
+if __name__ == "__main__":
+    main()
 
